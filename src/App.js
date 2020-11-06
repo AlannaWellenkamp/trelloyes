80b8b8a2d98c7f70ed1b476be8504e688609d705
Alanna Wellenkamp <actuallyalanna@gmail.com>
2020-11-06 13:51:38 -0800

    components working properly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import './App.css';
+import List from './composition/List';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends React.Component {
+
+
+  render() {
+    const { store } = this.props;
+    return (
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className='App-list'>
+          {store.lists.map(list => (
+            <List
+              header={list.header}
+              key={list.id}
+              cards={list.cardIds.map(id => store.allCards[id])}
+            />
+          ))}
+        </div>
+      </main>
+    )
+  }
+
 }
 
 export default App;
